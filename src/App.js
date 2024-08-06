b6650b3f274964d03c8ad7ceaed4716a359255b4
“Foxshane997” <Foxshane997@gmail.com>
2024-08-05 18:33:12 -0700

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import CountdownTimer from "./components/CountdownTimer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <CountdownTimer />
+      <Footer />
     </div>
   );
 }
